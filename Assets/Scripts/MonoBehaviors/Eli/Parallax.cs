c10f4252590fff0643255b1ae3a07252821e4548
Stick-2357 <emonnig10@gmail.com>
2022-03-18 13:50:00 -0500

    Add some tokens

--- Assets/Scripts/MonoBehaviors/Eli/Parallax.cs
+++ Assets/Scripts/MonoBehaviors/Eli/Parallax.cs
@@ -2,24 +2,25 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    public Camera cam;
+    private float _length, _startPos;
+    private Camera _cam;
     public float parallaxEffect;
 
     private void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _cam = Camera.main;
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void Update()
     {
-        var temp = cam.transform.position.x * (1 - parallaxEffect);
-        var distance = cam.transform.position.x * parallaxEffect;
+        var temp = _cam.transform.position.x * (1 - parallaxEffect);
+        var distance = _cam.transform.position.x * parallaxEffect;
 
-        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        if (temp > _startPos + _length) _startPos += _length;
+        else if (temp < _startPos - _length) _startPos -= _length;
     }
 }
