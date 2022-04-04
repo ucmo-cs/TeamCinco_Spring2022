be90758a395d52293963e0865ac92bca25e7f05e
alexfitz1021 <aef87340@ucmo.edu>
2022-04-04 18:53:49 -0500

    Improve camera var

--- Assets/Scripts/Alex/AlexCamFollow.cs
+++ Assets/Scripts/Alex/AlexCamFollow.cs
@@ -8,6 +8,12 @@ public class AlexCamFollow : MonoBehaviour
     private Vector3 _freezePoint;
 
     private bool _grabbed;
+    private Camera cam;
+
+    public void Start()
+    {
+        cam = Camera.main;
+    }
 
     // LateUpdate is called once per frame, right before it renders.
     private void LateUpdate()
@@ -30,7 +36,7 @@ public class AlexCamFollow : MonoBehaviour
 
         if (_grabbed)
         {
-            Vector2 mouseDistance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - cameraPos;
+            Vector2 mouseDistance = cam.ScreenToWorldPoint(Input.mousePosition) - cameraPos;
             var targetX = _freezePoint.x + mouseDistance.x / 10;
             var targetY = _freezePoint.y + mouseDistance.y / 10;
             var xDif = (targetX - cameraPos.x) * Time.deltaTime * 10;
