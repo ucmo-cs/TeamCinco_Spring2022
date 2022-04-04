368e8f123b9a14145a947ff2557cac5311e04af3
Nathan Law <nateforlaw@gmail.com>
2022-04-03 19:43:32 -0500

    Renamed some of my stuff, to avoid conflicts
    pls dont be mad i did this on someone elses branch oh no

--- Assets/Scripts/Nathan/Level1/NathanLV1DeathZone.cs
+++ Assets/Scripts/Nathan/Level1/NathanLV1DeathZone.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class DeathZoner : MonoBehaviour
+public class NathanLV1DeathZone : MonoBehaviour
 {
     public GameObject player;
 
     private void OnCollisionEnter2D(Collision2D targetObj)
     {
         if (targetObj.gameObject != player) return;
-        player.GetComponent<Respawnn>().Respawn();
+        player.GetComponent<NathanLV1Respawn>().Respawn();
     }
 }
