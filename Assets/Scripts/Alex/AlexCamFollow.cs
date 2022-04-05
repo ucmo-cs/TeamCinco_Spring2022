c13a0531dd1d2ca0fd826bceb763411bb30b500b
Stick-2357 <emonnig10@gmail.com>
2022-04-04 20:11:18 -0500

    Rewrite CamFollow

--- Assets/Scripts/Alex/AlexCamFollow.cs
+++ Assets/Scripts/Alex/AlexCamFollow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AlexCamFollow : MonoBehaviour
@@ -20,36 +18,19 @@ public class AlexCamFollow : MonoBehaviour
     {
         Vector2 objPos = objectOfInterest.transform.position;
         var cameraPos = gameObject.transform.position;
-        bool _isGrabbed = objectOfInterest.GetComponent<AlexPlayerMovement>().IsGrabbed();
         //Saving for Level 2
+        // bool _isGrabbed = objectOfInterest.GetComponent<AlexPlayerMovement>().IsGrabbed();
         // bool _isWaiting = objectOfInterest.GetComponent<AlexPlayerMovement>().IsWaiting();
         // if (!_grabbed && _isGrabbed && _isWaiting)
-        if (!_grabbed)
-        {
-            _freezePoint = gameObject.transform.position;
-            _grabbed = true;
-        }
-        //Saving for Level 2
         // else if (_grabbed && !_isGrabbed && !_isWaiting)
-        else if (_grabbed)
-        {
-            _grabbed = false;
-        }
-
-        if (_grabbed)
-        {
-            Vector2 mouseDistance = cam.ScreenToWorldPoint(Input.mousePosition) - cameraPos;
-            var targetX = _freezePoint.x + mouseDistance.x / 10;
-            var targetY = _freezePoint.y + mouseDistance.y / 10;
-            var xDif = (targetX - cameraPos.x) * Time.deltaTime * 10;
-            var yDif = (targetY - cameraPos.y) * Time.deltaTime * 10;
-            gameObject.transform.position = new Vector3(cameraPos.x + xDif, cameraPos.y + yDif, cameraPos.z);
-        }
-        else
-        {
-            var xDif = (objPos.x - cameraPos.x) * Time.deltaTime * 10;
-            var yDif = (objPos.y - cameraPos.y) * Time.deltaTime * 10;
-            gameObject.transform.position = new Vector3(cameraPos.x + xDif, cameraPos.y + yDif, cameraPos.z);
-        }
+        if (!_grabbed) _freezePoint = gameObject.transform.position;
+        _grabbed = !_grabbed;
+        
+        Vector2 mouseDistance = cam.ScreenToWorldPoint(Input.mousePosition) - cameraPos;
+        var targetX = _grabbed ? _freezePoint.x + mouseDistance.x / 10 : objPos.x;
+        var targetY = _grabbed ? _freezePoint.y + mouseDistance.y / 10 : objPos.y;
+        var xDif = (targetX - cameraPos.x) * Time.deltaTime * 10;
+        var yDif = (targetY - cameraPos.y) * Time.deltaTime * 10;
+        gameObject.transform.position = new Vector3(cameraPos.x + xDif, cameraPos.y + yDif, cameraPos.z);
     }
 }
